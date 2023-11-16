a8511582ed4108853b09e8646584a1083e2049e0
dipenbhat557 <dipenbhat557@gmail.com>
2023-11-16 13:23:39 +0530

    :bug: fixed the image problem of academic and administrative faculties

--- server/wp-content/plugins/academic-team-plugin.php
+++ server/wp-content/plugins/academic-team-plugin.php
@@ -1,17 +1,17 @@
 <?php
+
 /**
- * @package academic plugin
+ * @package Academic plugin
  * @version 1.0.0
  */
 /*
 Plugin Name: Academic plugin
 Plugin URI: http://deukhurimultiplecampus.edu.np/
-Description: Plugin for managing Academics with title and image.
+Description: The beginning of an awesome plugin
 Author: Me
 Version: 1.0.0
 Author URI: http://deukhurimultiplecampus.edu.np/
 */
-
 function generate_academic_type() {
     $labels = array(
         'name'                  => 'Academics',
@@ -24,48 +24,50 @@ function generate_academic_type() {
         'taxonomies'            => array( 'category', 'post_tag' ),
         'hierarchical'          => false,
         'public'                => true,
-        'capability_type'       => 'post',
+        'capability_type'       => 'post', // Change 'page' to 'post'
         'show_in_rest'          => true,
         'rest_base'             => 'academics',
-        'register_meta_box_cb'  => 'add_academic_details_meta_box', 
     );
     register_post_type( 'academic_type', $args );
 }
 add_action( 'init', 'generate_academic_type', 0 );
 
-// Add a meta box for the academic details
-function add_academic_details_meta_box() {
+
+// Add a meta box for the academic image
+function add_academic_image_meta_box() {
     add_meta_box(
-        'academic_details_meta_box',
-        'Academic Details',
-        'render_academic_details_meta_box',
+        'academic_image_meta_box',
+        'Academic Image',
+        'render_academic_image_meta_box',
         'academic_type',
         'normal',
         'high'
     );
 }
 
-// Render the content of the academic details meta box
-function render_academic_details_meta_box($post) {
-    // Get the current values if they exist
-    $academic_image_url = get_post_meta($post->ID, '_academic_image_url', true);
+add_action('add_meta_boxes', 'add_academic_image_meta_box');
+
+
+// Render the content of the academic image meta box
+function render_academic_image_meta_box($post) {
+    // Get the current image URL if it exists
+    $image_url = get_post_meta($post->ID, '_academic_image_url', true);
 
     // Use nonce for verification
-    wp_nonce_field(basename(__FILE__), 'academic_details_nonce');
+    wp_nonce_field(basename(__FILE__), 'academic_image_nonce');
 
-    // Display the inputs for the academic details
+    // Display the input for the academic image
     ?>
-    
-    <label for="academic_image">Upload Academic Image:</label>
-    <input type="text" name="academic_image" id="academic_image" class="widefat" value="<?php echo esc_url($academic_image_url); ?>" />
+    <label for="academic_image">Upload Academic Faculty Image:</label>
+    <input type="text" name="academic_image" id="academic_image" class="widefat" value="<?php echo esc_url($image_url); ?>" />
     <p>
-        <input type="button" id="upload_academic_image_button" class="button" value="Upload Image" />
+        <input type="button" id="upload_image_button" class="button" value="Upload Image" />
     </p>
     <script>
         jQuery(document).ready(function($){
             var custom_uploader;
 
-            $('#upload_academic_image_button').click(function(e) {
+            $('#upload_image_button').click(function(e) {
                 e.preventDefault();
 
                 //If the uploader object has already been created, reopen the dialog
@@ -97,10 +99,10 @@ function render_academic_details_meta_box($post) {
     <?php
 }
 
-// Save the academic details when saving the academic
-function save_academic_details_meta($post_id) {
+// Save the academic image URL when saving the academic
+function save_academic_image_meta($post_id) {
     // Verify nonce
-    if (!isset($_POST['academic_details_nonce']) || !wp_verify_nonce($_POST['academic_details_nonce'], basename(__FILE__))) {
+    if (!isset($_POST['academic_image_nonce']) || !wp_verify_nonce($_POST['academic_image_nonce'], basename(__FILE__))) {
         return $post_id;
     }
 
@@ -111,10 +113,9 @@ function save_academic_details_meta($post_id) {
 
     // Check the user's permissions
     if ('academic_type' == $_POST['post_type'] && current_user_can('edit_post', $post_id)) {
-        // Save academic name
-        update_post_meta($post_id, '_academic_name', sanitize_text_field($_POST['academic_name']));
-
         // Save academic image URL
         update_post_meta($post_id, '_academic_image_url', esc_url($_POST['academic_image']));
     }
 }
+
+add_action('save_post', 'save_academic_image_meta');
--- server/wp-content/plugins/administrative-plugin.php
+++ server/wp-content/plugins/administrative-plugin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package Administrative plugin
  * @version 1.0.0
@@ -6,12 +7,11 @@
 /*
 Plugin Name: Administrative plugin
 Plugin URI: http://deukhurimultiplecampus.edu.np/
-Description: Plugin for managing Administratives with title and image.
+Description: The beginning of an awesome plugin
 Author: Me
 Version: 1.0.0
 Author URI: http://deukhurimultiplecampus.edu.np/
 */
-
 function generate_administrative_type() {
     $labels = array(
         'name'                  => 'Administratives',
@@ -24,48 +24,50 @@ function generate_administrative_type() {
         'taxonomies'            => array( 'category', 'post_tag' ),
         'hierarchical'          => false,
         'public'                => true,
-        'capability_type'       => 'post',
+        'capability_type'       => 'post', // Change 'page' to 'post'
         'show_in_rest'          => true,
         'rest_base'             => 'administratives',
-        'register_meta_box_cb'  => 'add_administrative_details_meta_box', 
     );
     register_post_type( 'administrative_type', $args );
 }
 add_action( 'init', 'generate_administrative_type', 0 );
 
-// Add a meta box for the administrative details
-function add_administrative_details_meta_box() {
+
+// Add a meta box for the administrative image
+function add_administrative_image_meta_box() {
     add_meta_box(
-        'administrative_details_meta_box',
-        'Administrative Details',
-        'render_administrative_details_meta_box',
+        'administrative_image_meta_box',
+        'Administrative Image',
+        'render_administrative_image_meta_box',
         'administrative_type',
         'normal',
         'high'
     );
 }
 
-// Render the content of the administrative details meta box
-function render_administrative_details_meta_box($post) {
-    // Get the current values if they exist
-    $administrative_image_url = get_post_meta($post->ID, '_administrative_image_url', true);
+add_action('add_meta_boxes', 'add_administrative_image_meta_box');
+
+
+// Render the content of the administrative image meta box
+function render_administrative_image_meta_box($post) {
+    // Get the current image URL if it exists
+    $image_url = get_post_meta($post->ID, '_administrative_image_url', true);
 
     // Use nonce for verification
-    wp_nonce_field(basename(__FILE__), 'administrative_details_nonce');
+    wp_nonce_field(basename(__FILE__), 'administrative_image_nonce');
 
-    // Display the inputs for the administrative details
+    // Display the input for the administrative image
     ?>
-    
-    <label for="administrative_image">Upload Administrative Image:</label>
-    <input type="text" name="administrative_image" id="administrative_image" class="widefat" value="<?php echo esc_url($administrative_image_url); ?>" />
+    <label for="administrative_image">Upload Administrative Faculty Image:</label>
+    <input type="text" name="administrative_image" id="administrative_image" class="widefat" value="<?php echo esc_url($image_url); ?>" />
     <p>
-        <input type="button" id="upload_administrative_image_button" class="button" value="Upload Image" />
+        <input type="button" id="upload_image_button" class="button" value="Upload Image" />
     </p>
     <script>
         jQuery(document).ready(function($){
             var custom_uploader;
 
-            $('#upload_administrative_image_button').click(function(e) {
+            $('#upload_image_button').click(function(e) {
                 e.preventDefault();
 
                 //If the uploader object has already been created, reopen the dialog
@@ -97,10 +99,10 @@ function render_administrative_details_meta_box($post) {
     <?php
 }
 
-// Save the administrative details when saving the administrative
-function save_administrative_details_meta($post_id) {
+// Save the administrative image URL when saving the administrative
+function save_administrative_image_meta($post_id) {
     // Verify nonce
-    if (!isset($_POST['administrative_details_nonce']) || !wp_verify_nonce($_POST['administrative_details_nonce'], basename(__FILE__))) {
+    if (!isset($_POST['administrative_image_nonce']) || !wp_verify_nonce($_POST['administrative_image_nonce'], basename(__FILE__))) {
         return $post_id;
     }
 
@@ -111,10 +113,9 @@ function save_administrative_details_meta($post_id) {
 
     // Check the user's permissions
     if ('administrative_type' == $_POST['post_type'] && current_user_can('edit_post', $post_id)) {
-        // Save administrative name
-        update_post_meta($post_id, '_administrative_name', sanitize_text_field($_POST['administrative_name']));
-
         // Save administrative image URL
         update_post_meta($post_id, '_administrative_image_url', esc_url($_POST['administrative_image']));
     }
 }
+
+add_action('save_post', 'save_administrative_image_meta');
