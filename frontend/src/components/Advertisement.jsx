07025ee347717cdc17839359a7e4b9f3fa692c0d
Avashneupane9857 <juisch20220011252@gmail.com>
2024-03-02 18:31:29 +0530

    HomePageNotice Updated

--- frontend/src/components/Advertisement.jsx
+++ frontend/src/components/Advertisement.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Notice.css";
 
-const Advertisement = () => {
-  return <div>Advertisement</div>;
-};
+import { FaTimes } from "react-icons/fa";
+import useFetch from "./UseFetch";
+function Notice({ setShowNotice }) {
+  const advertisements = useFetch(
+    `${import.meta.env.VITE_APP_API_ROOT}/advertisements`
+  );
+  return (
+    <div className="notice-container">
+      <div className="notice-content">
+        <img
+          src={advertisements?.[0]?.imageUrl}
+          alt="Notice"
+          className="notice-image"
+        />
+        <div className="btn">
+          <button onClick={() => setShowNotice(false)}>
+            {" "}
+            <FaTimes /> {/* Use the icon component */}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Advertisement() {
+  const [showNotice, setShowNotice] = useState(true);
+
+  return (
+    <div className="home-page">
+      {showNotice && <div className="background-blur"></div>}
+      {showNotice && <Notice setShowNotice={setShowNotice} />}
+    </div>
+  );
+}
 
 export default Advertisement;
