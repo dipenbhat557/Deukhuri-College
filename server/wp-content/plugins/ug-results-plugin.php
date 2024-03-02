b5465c39fa9c005629b9393eb3f2a270a66149a9
dipenbhat557 <dipenbhat557@gmail.com>
2024-03-02 18:18:01 +0530

    :bug: fixed not uploading error

--- server/wp-content/plugins/ug-results-plugin.php
+++ server/wp-content/plugins/ug-results-plugin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package UG Result plugin
  * @version 1.0.0
@@ -6,7 +7,7 @@
 /*
 Plugin Name: UG Result plugin
 Plugin URI: http://deukhurimultiplecampus.edu.np/
-Description: Plugin for uploading undergraduate results with a PDF input field.
+Description: Plugin for uploading undergraduate results with an image input field.
 Author: Me
 Version: 1.0.0
 Author URI: http://deukhurimultiplecampus.edu.np/
@@ -32,40 +33,40 @@ function generate_ug_result_type() {
 }
 add_action( 'init', 'generate_ug_result_type', 0 );
 
-// Add a meta box for the PDF upload
-function add_ug_result_pdf_meta_box() {
+// Add a meta box for the image upload
+function add_ug_result_image_meta_box() {
     add_meta_box(
-        'ug_result_pdf_meta_box',
-        'UG Result PDF',
-        'render_ug_result_pdf_meta_box',
+        'ug_result_image_meta_box',
+        'UG Result Image',
+        'render_ug_result_image_meta_box',
         'ug_result_type',
         'normal',
         'high'
     );
 }
 
-add_action('add_meta_boxes', 'add_ug_result_pdf_meta_box');
+add_action('add_meta_boxes', 'add_ug_result_image_meta_box');
 
-// Render the content of the PDF upload meta box
-function render_ug_result_pdf_meta_box($post) {
-    // Get the current PDF URL if it exists
-    $pdf_url = get_post_meta($post->ID, '_ug_result_pdf_url', true);
+// Render the content of the image upload meta box
+function render_ug_result_image_meta_box($post) {
+    // Get the current image URL if it exists
+    $image_url = get_post_meta($post->ID, '_ug_result_image_url', true);
 
     // Use nonce for verification
-    wp_nonce_field(basename(__FILE__), 'ug_result_pdf_nonce');
+    wp_nonce_field(basename(__FILE__), 'ug_result_image_nonce');
 
-    // Display the input for the PDF upload
+    // Display the input for the image upload
     ?>
-    <label for="ug_result_pdf">Upload UG Result PDF:</label>
-    <input type="text" name="ug_result_pdf" id="ug_result_pdf" class="widefat" value="<?php echo esc_url($pdf_url); ?>" />
+    <label for="ug_result_image">Upload UG Result Image:</label>
+    <input type="text" name="ug_result_image" id="ug_result_image" class="widefat" value="<?php echo esc_url($image_url); ?>" />
     <p>
-        <input type="button" id="upload_pdf_button" class="button" value="Upload PDF" />
+        <input type="button" id="upload_image_button" class="button" value="Upload Image" />
     </p>
     <script>
         jQuery(document).ready(function($){
             var custom_uploader;
 
-            $('#upload_pdf_button').click(function(e) {
+            $('#upload_image_button').click(function(e) {
                 e.preventDefault();
 
                 // If the uploader object has already been created, reopen the dialog
@@ -76,9 +77,9 @@ function render_ug_result_pdf_meta_box($post) {
 
                 // Extend the wp.media object
                 custom_uploader = wp.media.frames.file_frame = wp.media({
-                    title: 'Choose PDF',
+                    title: 'Choose Image',
                     button: {
-                        text: 'Choose PDF'
+                        text: 'Choose Image'
                     },
                     multiple: false
                 });
@@ -86,7 +87,7 @@ function render_ug_result_pdf_meta_box($post) {
                 // When a file is selected, grab the URL and set it as the text field's value
                 custom_uploader.on('select', function() {
                     var attachment = custom_uploader.state().get('selection').first().toJSON();
-                    $('#ug_result_pdf').val(attachment.url);
+                    $('#ug_result_image').val(attachment.url);
                 });
 
                 // Open the uploader dialog
@@ -97,10 +98,10 @@ function render_ug_result_pdf_meta_box($post) {
     <?php
 }
 
-// Save the UG Result PDF URL when saving the UG Result
-function save_ug_result_pdf_meta($post_id) {
+// Save the UG Result image URL when saving the UG Result
+function save_ug_result_image_meta($post_id) {
     // Verify nonce
-    if (!isset($_POST['ug_result_pdf_nonce']) || !wp_verify_nonce($_POST['ug_result_pdf_nonce'], basename(__FILE__))) {
+    if (!isset($_POST['ug_result_image_nonce']) || !wp_verify_nonce($_POST['ug_result_image_nonce'], basename(__FILE__))) {
         return $post_id;
     }
 
@@ -111,9 +112,11 @@ function save_ug_result_pdf_meta($post_id) {
 
     // Check the user's permissions
     if ('ug_result_type' == $_POST['post_type'] && current_user_can('edit_post', $post_id)) {
-        // Save UG Result PDF URL
-        update_post_meta($post_id, '_ug_result_pdf_url', esc_url($_POST['ug_result_pdf']));
+        // Save UG Result image URL
+        update_post_meta($post_id, '_ug_result_image_url', esc_url($_POST['ug_result_image']));
     }
 }
 
-add_action('save_post', 'save_ug_result_pdf_meta');
+add_action('save_post', 'save_ug_result_image_meta');
+
+
