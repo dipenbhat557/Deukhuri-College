659bc8b35bafa1d1bba72c205afabf46d9a3f3e5
Dipendra Bhatta <dipenbhat557@gmail.com>
2024-07-06 17:55:39 +0530

    :sparkles: added spring boot backend to project

---
+++ backend/src/main/java/com/dmc/DmcApplication.java
@@ -0,0 +1,13 @@
+package com.dmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmcApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dmc/DmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
