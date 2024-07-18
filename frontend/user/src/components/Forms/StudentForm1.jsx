bae255f5ba70ce878eecee865239dbedbc559a34
Avashneupane9857 <juisch20220011252@gmail.com>
2024-07-18 22:47:19 +0530

    :wrench: Forms data updation

--- frontend/user/src/components/Forms/StudentForm1.jsx
+++ frontend/user/src/components/Forms/StudentForm1.jsx
@@ -22,16 +22,23 @@ const StudentForm1 = () => {
   };
 
   useEffect(() => {
-    
-    const requiredFields = ["program", "academicYear", "yearSemester", "medium", "majorSubject", "shift"];
-    const isValid = requiredFields.every((field) => formData[field].trim() !== "");
+    const requiredFields = [
+      "program",
+      "acdmc_year",
+      "year_semester",
+      "medium",
+      "shift",
+    ];
+    const isValid = requiredFields.every(
+      (field) => formData[field].trim() !== ""
+    );
     setIsFormValid(isValid);
     setError(!isValid);
   }, [formData]);
 
-  useEffect(()=>{
-    window.scrollTo(0,0);
-  },[])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   const handleNext = () => {
     navigate("/form2");
@@ -41,13 +48,37 @@ const StudentForm1 = () => {
     <>
       <HeroHeader />
       <div className="p-8 w-full bg-gray-100 rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold mb-6">Personal and Academic Information</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">{error && <p className="text-red-800 text-md">Fill all the compulsory fields first!!</p>}
-          <p className="text-red-800 text-md">Fields with (*) are compulsory.</p></div>
+        <h2 className="text-2xl font-bold mb-6">
+          Personal and Academic Information
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          {error && (
+            <p className="text-red-800 text-md">
+              Fill all the compulsory fields first!!
+            </p>
+          )}
+          <p className="text-red-800 text-md">
+            Fields with (*) are compulsory.
+          </p>
+        </div>
         <form className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          
           <div className="mb-4">
-            <label className="block mb-2">Program<span className="text-red-500">*</span></label>
+            <label className="block mb-2">
+              Program<span className="text-red-500">*</span>
+            </label>
+            <select
+              name="program"
+              value={formData.program}
+              onChange={handleChange}
+              required
+              className="select"
+            >
+              <option value={1}>BBS</option>
+              <option value={2}>MBS</option>
+              <option value={3}>BA</option>
+              <option value={4}>B.Ed</option>
+              <option value={5}>M.Ed</option>
+            </select>
             <input
               className="p-2 w-full border border-gray-300 rounded"
               type="text"
@@ -58,29 +89,72 @@ const StudentForm1 = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Academic Year<span className="text-red-500">*</span></label>
+            <label className="block mb-2">
+              Academic Year<span className="text-red-500">*</span>
+            </label>
+            <select
+              className="select"
+              name="acdmc_year"
+              value={formData.academicYear}
+              onChange={handleChange}
+              required
+            >
+              <option value={1}>2080-2084</option>
+              <option value={2}>2079-2083</option>
+              <option value={3}>2078-2082</option>
+              <option value={4}>2077-2081</option>
+            </select>
             <input
               className="p-2 w-full border border-gray-300 rounded"
               type="text"
-              name="academicYear"
+              name="acdmc_year"
               value={formData.academicYear}
               onChange={handleChange}
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Year/Semester<span className="text-red-500">*</span></label>
+            <label className="block mb-2">
+              Year/Semester<span className="text-red-500">*</span>
+            </label>
+            <select
+              className="select"
+              name="year_semester"
+              value={formData.yearSemester}
+              onChange={handleChange}
+              required
+            >
+              <option value="First Year">First Year</option>
+              <option value="Second Year">Second Year</option>
+              <option value="Third Year">Third Year</option>
+              <option value="Fourth Year">Fourth Year</option>
+              <option value="Complete Year">Complete Year</option>
+            </select>
+
             <input
               className="p-2 w-full border border-gray-300 rounded"
               type="text"
-              name="yearSemester"
+              name="year_semester"
               value={formData.yearSemester}
               onChange={handleChange}
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Medium<span className="text-red-500">*</span></label>
+            <label className="block mb-2">
+              Medium<span className="text-red-500">*</span>
+            </label>
+            <select
+              className="select"
+              name="medium"
+              value={formData.medium}
+              onChange={handleChange}
+              required
+            >
+              <option value={1}>English</option>
+              <option value={2}>Nepali</option>
+            </select>
+
             <input
               className="p-2 w-full border border-gray-300 rounded"
               type="text"
@@ -91,18 +165,40 @@ const StudentForm1 = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Section</label>
+            <label className="block mb-2">
+              Section<span className="text-red-500">*</span>
+            </label>
+            <select
+              className="select"
+              name="sec"
+              value={formData.section}
+              onChange={handleChange}
+              required
+            >
+              <option value={1}>No Section</option>
+            </select>
             <input
               className="p-2 w-full border border-gray-300 rounded"
               type="text"
-              name="section"
+              name="sec"
               value={formData.section}
               onChange={handleChange}
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Team</label>
+            <label className="block mb-2">
+              Team <span className="text-red-500"></span>
+            </label>
+            <select
+              className="select"
+              name="team"
+              value={formData.team}
+              onChange={handleChange}
+              required
+            >
+              <option value={1}>No Team</option>
+            </select>
             <input
               className="p-2 w-full border border-gray-300 rounded"
               type="text"
@@ -113,7 +209,20 @@ const StudentForm1 = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Shift<span className="text-red-500">*</span></label>
+            <label className="block mb-2">
+              Shift<span className="text-red-500">*</span>
+            </label>
+            <select
+              className="select"
+              name="shift"
+              value={formData.shift}
+              onChange={handleChange}
+              required
+            >
+              <option value={1}>Morning</option>
+              <option value={2}>Evening</option>
+              <option value={3}>Night</option>
+            </select>
             <input
               className="p-2 w-full border border-gray-300 rounded"
               type="text"
@@ -124,11 +233,18 @@ const StudentForm1 = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block mb-2">Major Subject <span className="text-red-500">*</span></label>
+            <label className="block mb-2">Major Subject </label>
+            <select
+              name="maj_subj"
+              value={formData.majorSubject}
+              onChange={handleChange}
+            >
+              <option value={1}></option>
+            </select>
             <input
               className="p-2 w-full border border-gray-300 rounded"
               type="text"
-              name="majorSubject"
+              name="maj_subj"
               value={formData.majorSubject}
               onChange={handleChange}
             />
@@ -136,7 +252,11 @@ const StudentForm1 = () => {
         </form>
         <div className="flex w-full justify-end">
           <button
-            className={`py-2 w-[25%] md:w-[10%] flex items-center justify-center gap-3 ${isFormValid ? "bg-blue-500 hover:bg-blue-700 cursor-pointer" : "bg-gray-500"} text-white rounded`}
+            className={`py-2 w-[25%] md:w-[10%] flex items-center justify-center gap-3 ${
+              isFormValid
+                ? "bg-blue-500 hover:bg-blue-700 cursor-pointer"
+                : "bg-gray-500"
+            } text-white rounded`}
             type="button"
             onClick={handleNext}
             disabled={!isFormValid}
--- frontend/user/src/components/Forms/StudentForm2.jsx
+++ frontend/user/src/components/Forms/StudentForm2.jsx
@@ -21,18 +21,38 @@ const StudentForm2 = () => {
 
   useEffect(() => {
     const requiredFields = [
-      "firstName", "lastName", "nameNepali", "dateOfBirthBS", "phoneNumber",
-      "email", "maritalStatus", "gender", "ethnicity", "caste", "religion",
-      "province", "district", "municipality", "wardNo", "smsNo", "fatherName",
-      "fatherQualification", "motherName", "motherQualification"
+      "first_name",
+      "last_name",
+      "name_nep",
+      "dobn",
+      "phone",
+      ,
+      "marital_status",
+      "gender",
+      "sub_caste",
+      "caste",
+      "religion",
+      "nationality",
+
+      "province",
+      "district",
+      "municipality",
+      "wardno",
+      "sms_mob_no",
+      "father_name",
+      "father_qualification",
+      "mother_name",
+      "mother_qualification",
     ];
-    const isValid = requiredFields.every((field) => formData[field].trim() !== "");
+    const isValid = requiredFields.every(
+      (field) => formData[field].trim() !== ""
+    );
     setIsFormValid(isValid);
     setError(!isValid);
   }, [formData]);
-  useEffect(()=>{
-    window.scrollTo(0,0);
-  },[])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -41,40 +61,128 @@ const StudentForm2 = () => {
 
   return (
     <div className="p-8 w-full bg-gray-100 rounded-lg shadow-md">
-      <h3 className="text-2xl font-bold mb-6 sm:col-span-2">Student Information</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">{error && <p className="text-red-800 text-md">Fill all the compulsory fields first!!</p>}
-          <p className="text-red-800 text-md">Fields with (*) are compulsory.</p></div>
-        
-      <form  className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <h3 className="text-2xl font-bold mb-6 sm:col-span-2">
+        Student Information
+      </h3>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {error && (
+          <p className="text-red-800 text-md">
+            Fill all the compulsory fields first!!
+          </p>
+        )}
+        <p className="text-red-800 text-md">Fields with (*) are compulsory.</p>
+      </div>
+
+      <form className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         {[
-          { label: "First Name", name: "firstName", type: "text", required: true },
-          { label: "Middle Name", name: "middleName", type: "text" },
-          { label: "Last Name", name: "lastName", type: "text", required: true },
-          { label: "Name (Nepali)", name: "nameNepali", type: "text", required: true },
-          { label: "Date of Birth (BS)", name: "dateOfBirthBS", type: "date", required: true },
-          { label: "Phone Number", name: "phoneNumber", type: "tel", required: true },
-          { label: "E-mail", name: "email", type: "email", required: true },
-          { label: "Marital Status", name: "maritalStatus", type: "select", options: ["", "Single", "Married"], required: true },
-          { label: "Gender", name: "gender", type: "select", options: ["", "Male", "Female", "Other"], required: true },
-          { label: "Ethnicity", name: "ethnicity", type: "text", required: true },
-          { label: "Caste", name: "caste", type: "select", options: ["","Brahmin","Chetri"] /* Add options here */, required: true },
-          { label: "Blood Group", name: "bloodGroup", type: "text" },
-          { label: "Citizenship No", name: "citizenshipNo", type: "text" },
+          {
+            label: "First Name",
+            name: "first_name",
+            type: "text",
+            required: true,
+          },
+          { label: "Middle Name", name: "middle_name", type: "text" },
+          {
+            label: "Last Name",
+            name: "last_name",
+            type: "text",
+            required: true,
+          },
+          {
+            label: "Name (Nepali)",
+            name: "name_nep",
+            type: "text",
+            required: true,
+          },
+          {
+            label: "Date of Birth (BS)",
+            name: "dobn",
+            type: "date",
+            required: true,
+          },
+          {
+            label: "Phone Number",
+            name: "phone",
+            type: "tel",
+            required: true,
+          },
+          { label: "E-mail", name: "email", type: "email" },
+          {
+            label: "Marital Status",
+            name: "marital_status",
+            type: "select",
+            options: ["", "Single", "Married"],
+            required: true,
+          },
+          {
+            label: "Gender",
+            name: "gender",
+            type: "select",
+            options: ["", "Male", "Female", "Other"],
+            required: true,
+          },
+          {
+            label: "Ethnicity",
+            name: "sub_caste",
+            type: "text",
+            required: true,
+          },
+          {
+            label: "Caste",
+            name: "caste",
+            type: "select",
+            options: ["", "Brahmin", "Chetri"] /* Add options here */,
+            required: true,
+          },
+          { label: "Blood Group", name: "bloodgroup", type: "text" },
+          { label: "Citizenship No", name: "citiz_no", type: "text" },
           { label: "Religion", name: "religion", type: "text", required: true },
-          { label: "Nationality", name: "nationality", type: "text", readOnly: true },
-          { label: "Province", name: "province", type: "select", options: ["","1","2","3","4","5"] /* Add options here */, required: true },
+          {
+            label: "Nationality",
+            name: "nationality",
+            type: "text",
+            readOnly: true,
+            required: true,
+          },
+          {
+            label: "Province",
+            name: "province",
+            type: "select",
+            options: ["", "1", "2", "3", "4", "5"] /* Add options here */,
+            required: true,
+          },
           { label: "District", name: "district", type: "text", required: true },
-          { label: "Municipality", name: "municipality", type: "text", required: true },
-          { label: "Ward No", name: "wardNo", type: "text", required: true },
-          { label: "Address (Nepali)", name: "addressNepali", type: "text" },
-          { label: "Temporary Address", name: "temporaryAddress", type: "text" },
-          { label: "SMS No", name: "smsNo", type: "text", required: true },
-          { label: "Dorm Facility", name: "dormFacility", type: "select", options: ["", "Yes", "No"] },
-          { label: "Bus Facility", name: "busFacility", type: "select", options: ["", "Yes", "No"] },
+          {
+            label: "Municipality",
+            name: "municipality",
+            type: "text",
+            required: true,
+          },
+          { label: "Ward No", name: "wardno", type: "text", required: true },
+          { label: "Address (Nepali)", name: "add_nep", type: "text" },
+          {
+            label: "Temporary Address",
+            name: "tempadd",
+            type: "text",
+          },
+          { label: "SMS No", name: "sms_mob_no", type: "text", required: true },
+          {
+            label: "Dorm Facility",
+            name: "dorm_facility",
+            type: "select",
+            options: ["", "Yes", "No"],
+          },
+          {
+            label: "Bus Facility",
+            name: "bus_facility",
+            type: "select",
+            options: ["", "Yes", "No"],
+          },
         ].map((field, index) => (
           <div key={index} className="mb-4">
             <label className="block mb-2">
-              {field.label}{field.required && <span className="text-red-500">*</span>}
+              {field.label}
+              {field.required && <span className="text-red-500">*</span>}
             </label>
             {field.type === "select" ? (
               <select
@@ -85,7 +193,9 @@ const StudentForm2 = () => {
                 required={field.required}
               >
                 {field.options.map((option, i) => (
-                  <option key={i} value={option.toLowerCase()}>{option}</option>
+                  <option key={i} value={option.toLowerCase()}>
+                    {option}
+                  </option>
                 ))}
               </select>
             ) : (
@@ -102,18 +212,43 @@ const StudentForm2 = () => {
           </div>
         ))}
 
-        <h3 className="text-2xl font-bold mb-6 sm:col-span-2 mt-8">Parent Information</h3>
+        <h3 className="text-2xl font-bold mb-6 sm:col-span-2 mt-8">
+          Parent Information
+        </h3>
         {[
-          { label: "Father Name", name: "fatherName", type: "text", required: true },
-          { label: "Father Phone No", name: "fatherPhoneNo", type: "tel" },
-          { label: "Father Qualification", name: "fatherQualification", type: "select", options: ["","BBS","BBA"] /* Add options here */, required: true },
-          { label: "Mother Name", name: "motherName", type: "text", required: true },
-          { label: "Mother Phone No", name: "motherPhoneNo", type: "tel" },
-          { label: "Mother Qualification", name: "motherQualification", type: "select", options: ["","BBS","BBA"] /* Add options here */, required: true },
+          {
+            label: "Father Name",
+            name: "father_name",
+            type: "text",
+            required: true,
+          },
+          { label: "Father Phone No", name: "fath_ph", type: "tel" },
+          {
+            label: "Father Qualification",
+            name: "father_qualification ",
+            type: "select",
+            options: ["", "BBS", "BBA"] /* Add options here */,
+            required: true,
+          },
+          {
+            label: "Mother Name",
+            name: "mother_name",
+            type: "text",
+            required: true,
+          },
+          { label: "Mother Phone No", name: "moth_ph", type: "tel" },
+          {
+            label: "Mother Qualification",
+            name: "mother_qualification",
+            type: "select",
+            options: ["", "BBS", "BBA"] /* Add options here */,
+            required: true,
+          },
         ].map((field, index) => (
           <div key={index} className="mb-4">
             <label className="block mb-2">
-              {field.label}{field.required && <span className="text-red-500">*</span>}
+              {field.label}
+              {field.required && <span className="text-red-500">*</span>}
             </label>
             {field.type === "select" ? (
               <select
@@ -124,7 +259,9 @@ const StudentForm2 = () => {
                 required={field.required}
               >
                 {field.options.map((option, i) => (
-                  <option key={i} value={option.toLowerCase()}>{option}</option>
+                  <option key={i} value={option.toLowerCase()}>
+                    {option}
+                  </option>
                 ))}
               </select>
             ) : (
@@ -139,8 +276,6 @@ const StudentForm2 = () => {
             )}
           </div>
         ))}
-      
-      
       </form>
       <div className="flex w-full justify-between">
         <button
@@ -152,7 +287,11 @@ const StudentForm2 = () => {
           Prev
         </button>
         <button
-          className={`py-2 w-[25%] md:w-[10%] flex items-center justify-center gap-3 ${isFormValid ? "bg-blue-500 hover:bg-blue-700 cursor-pointer" : "bg-gray-500"} text-white rounded`}
+          className={`py-2 w-[25%] md:w-[10%] flex items-center justify-center gap-3 ${
+            isFormValid
+              ? "bg-blue-500 hover:bg-blue-700 cursor-pointer"
+              : "bg-gray-500"
+          } text-white rounded`}
           onClick={handleSubmit}
           disabled={!isFormValid}
         >
