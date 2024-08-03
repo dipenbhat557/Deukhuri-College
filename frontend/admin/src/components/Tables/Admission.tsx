b18294a3d72bd81a20c2ecaab81ff0679118788d
dipenbhat557 <dipenbhat557@gmail.com>
2024-08-03 22:59:10 +0530

    :bug: fixed verify conflict

--- frontend/admin/src/components/Tables/Admission.tsx
+++ frontend/admin/src/components/Tables/Admission.tsx
@@ -129,7 +129,7 @@ const Admission = () => {
                       }
                       className={`${admission?.verified ? "bg-green-500 hover:bg-green-700" : "bg-blue-500 hover:bg-blue-700"} text-white font-bold py-2 px-4 rounded-full`}
                     >
-                      {admission?.verified ? "Verify":"Verified"}
+                      {admission?.verified ? "Verified":"Verify"}
                     </button>
                   </td>
                   <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
